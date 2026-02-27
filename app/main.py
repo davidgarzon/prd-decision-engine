b6f72cc30d837843eb1c1da87d09e48278bf9195
David Garzon <davidgarzon@gmail.com>
2026-02-27 22:53:32 +0100

    Cors added

--- app/main.py
+++ app/main.py
@@ -18,7 +18,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://prd-decision-engine-ewa84trba-davidgarzons-projects.vercel.app/",  
+     
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
